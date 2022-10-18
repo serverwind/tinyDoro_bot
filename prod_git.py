2fdfbcbc124208ee2c67f7820cd8a110e09edb28
Lestrangeqq <lestrangeqq@gmail.com>
2022-10-18 21:16:55 +0300

    Release of first version of the Bot!

--- prod_git.py
+++ prod_git.py
@@ -22,41 +22,46 @@ def help(message):
 # команда: установить таймер на 25 минут
 @bot.message_handler(commands=['add'])
 def setTimer(message):
+    regime = 1
     command = message.text.split(maxsplit=1)
     workMin = command[1]
-    bot.send_message(message.chat.id, 'Таймер установлен на ' + workMin + 'минут.')
+    bot.send_message(message.chat.id, 'Таймер установлен на ' + workMin + ' минут.')
+    workTimer(message, workMin, regime)
+
+
+# процесс переключения режимов таймера с работы на отдых
+def workTimer(message, workMin, regime):
     workSec = int(workMin)
-    regime = 1
-    status = True
-    while status:
-    # по прошествии 25 минут приходит уведомление о том,
-    # что нужно сделать перерыв на 5 минут.
-    # пользователь жмет ок и уходит на перерыв, таймер стартует.
-        if regime == 0:
+
+    if regime == 0:
+        bot.send_message(message.chat.id, 'Время отдохнуть! Введите "yaw" чтобы начать.')
+
+        @bot.message_handler(commands=['yaw'])
+        def startRest(message):
             regime = 1
-            bot.send_message(message.chat.id, 'Время отдохнуть! ' + str(status))
-            timer(5, regime)
-    # по прошествии 5 минут приходит уведомление, что нужно начать работу
-    # пользователь жмет ок, начинает работу, таймер стартует.
-        elif regime == 1:
+            timer(5, workMin, regime, message)
+    elif regime == 1:
+        bot.send_message(message.chat.id, 'Время поработать! Введите "yes" чтобы начать.')
+
+        @bot.message_handler(commands=['yes'])
+        def startWork(message):
             regime = 0
-            bot.send_message(message.chat.id, 'Время поработать! ' + str(status))
-            timer(workSec, regime)
+            timer(workSec, workMin, regime, message)
 
 
 # функция работы таймера
-def timer(workSec, regime):
+def timer(workSec, workMin, regime, message):
     while workSec > 0:
         time.sleep(1)
         workSec -= 1
-        print(workSec, 'regime: ', regime)  # для проверки на баги
+    workTimer(message, workMin, regime)
 
 
 @bot.message_handler(commands=['stop'])
 def stop(message):
     bot.send_message(message.chat.id, 'Таймер должен быть остановлен.')
-    status = False
-    print(status)
 
-#цикл запросов на сервер телеграм, чтобы бот проверял отправили ли ему сообщение
-bot.polling(none_stop = True)
+
+# цикл запросов на сервер телеграм, чтобы бот
+# проверял отправили ли ему сообщение
+bot.polling(none_stop=True)
